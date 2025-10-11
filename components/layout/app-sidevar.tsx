9698c1ad325be84d0e648492509b8b826dd3cbfe
taiccy <taishi.kpum@gmail.com>
2025-10-11 21:07:52 +0900

    レイアウト構造 shadcn-sidebar をベースに構築

---
+++ components/layout/app-sidevar.tsx
@@ -0,0 +1,224 @@
+import { ChevronDown, Home, Inbox, MapPin } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible"
+
+// コース・プラン
+const plans = [
+  {
+    title: "女子会・ママ会",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "宴会・飲み放題",
+    url: "#",
+    icon: Inbox,
+  },
+]
+
+// 通販クラブ
+const items = [
+  {
+    title: "商品一覧",
+    url: "/catalog",
+    icon: Inbox,
+  },
+  {
+    title: "おつまみ・珍味",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "越前がに",
+    url: "#",
+    icon: Home,
+  },
+]
+
+// メニュー
+const menus = [
+  {
+    title: "獲れたてのお魚",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "激戦お肉と季節の山野菜",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "おいしいご飯",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "定番おしながき",
+    url: "#",
+    icon: Home,
+  },
+]
+
+// メニュー
+const drinks = [
+  {
+    title: "和の果実酒",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "梅酒飲みくらべ",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "焼酎ラインナップ",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "おすすめ日本酒",
+    url: "#",
+    icon: Home,
+  },
+]
+export function AppSidebar() {
+  return (
+    <Sidebar side="right">
+      <SidebarContent>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                コース・プラン
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {plans.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <Link href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                通販クラブ
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <Link href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+        <Collapsible defaultOpen={false} className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                お食事
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {menus.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <Link href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+        <Collapsible defaultOpen={false} className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                お飲み物
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {drinks.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <Link href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="#">
+                <MapPin />
+                <span>アクセス・お問い合わせ</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
