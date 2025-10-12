0d9824da2f035b236d46f3654048d96a6d522c53
taiccy <taishi.kpum@gmail.com>
2025-10-12 20:22:49 +0900

    新規作成

---
+++ app/(shop)/access/page.tsx
@@ -0,0 +1,16 @@
+import PageTitle from "@/components/page/page-title"
+
+export default function Page() {
+  return (
+    <>
+      <PageTitle
+        title="アクセス・お問い合わせ"
+        subTitle="お気軽にお問い合わせください"
+      />
+      <div className="p-6 text-center">
+        準備中
+        <p>google Map 及び モバイル対応の電話発信ボタンを設置</p>
+      </div>
+    </>
+  )
+}
