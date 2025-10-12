8dca321a47bd45ee4cd051dc1ffd9d3777ca7107
taiccy <taishi.kpum@gmail.com>
2025-10-12 20:26:56 +0900

    ショッピングカート機能を追加

---
+++ components/layout/shopping/header-drawer-dialog.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { useIsMobile } from "@/hooks/use-mobile"
+import { cn } from "@/lib/utils"
+import { ShoppingCart } from "lucide-react"
+
+export default function ShoppingCartDrawerDialog() {
+  const [open, setOpen] = React.useState(false)
+  const isMobile = useIsMobile()
+
+  const title = "ショッピングカート"
+  const description = "カートの中身を確認・編集できます。"
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger>
+          <ShoppingCart />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <ProfileForm className="px-4" />
+          <DrawerFooter className="pt-2">
+            <DrawerClose asChild>
+              <Button variant="outline">閉じる</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <ShoppingCart />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <ProfileForm />
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+function ProfileForm({ className }: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("grid items-start gap-6", className)}>
+      <div className="text-muted-foreground flex h-44 items-center justify-center text-sm">
+        現在選択された商品はありません
+      </div>
+      <Button disabled type="submit">
+        購入手続きへ
+      </Button>
+    </form>
+  )
+}
