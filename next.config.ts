e3db21ed95915108417d8cb4b641df3ec6d856fd
taiccy <taishi.kpum@gmail.com>
2025-10-12 22:08:57 +0900

    リダイレクト対応を追加

--- next.config.ts
+++ next.config.ts
@@ -44,6 +44,12 @@ const redirectsList = [
     source: "/%E5%AE%9A%E7%95%AA%E3%81%8A%E3%81%97%E3%81%AA%E3%81%8C%E3%81%8D",
     destination: "/food/teiban",
   },
+  {
+    title: "越前がに 通販くらぶ",
+    source:
+      "/%E8%B6%8A%E5%89%8D%E3%81%8C%E3%81%AB/%E3%81%9B%E3%81%84%E3%81%93%E3%81%8C%E3%81%AB/%E3%81%9A%E3%82%8F%E3%81%84%E3%81%8C%E3%81%AB/%E9%80%9A%E8%B2%A9%E3%81%8F%E3%82%89%E3%81%B6",
+    destination: "/catalog/category/echizen-gani",
+  },
 ]
 
 const nextConfig: NextConfig = {
