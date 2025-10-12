a16b5008ba40d2ae959de8fcdfdbc122ea7baead
taiccy <taishi.kpum@gmail.com>
2025-10-12 20:27:15 +0900

    カニ商品を表示

--- app/(shop)/catalog/page.tsx
+++ app/(shop)/catalog/page.tsx
@@ -1,13 +1,13 @@
 import PageTitle from "@/components/page/page-title"
 import ItemsGrid from "./_components/items-grid"
-import { otsumamiItems, sabaItems } from "./category/items"
+import { kaniItems, otsumamiItems, sabaItems } from "./category/items"
 
 export default function Page() {
   return (
     <>
       <PageTitle title="お取り寄せ - 通販くらぶ" subTitle="商品一覧" />
       <section className="mx-2 my-10">
-        <ItemsGrid items={[...otsumamiItems, ...sabaItems]} />
+        <ItemsGrid items={[...otsumamiItems, ...sabaItems, ...kaniItems]} />
       </section>
     </>
   )
