99f892ad22fdb2241a9f3ca7ef041111ced7196b
taiccy <taishi.kpum@gmail.com>
2025-10-12 18:21:26 +0900

    タイトルを修正

--- app/(shop)/drink/syochu/page.tsx
+++ app/(shop)/drink/syochu/page.tsx
@@ -3,7 +3,7 @@ import PageTitle from "@/components/page/page-title"
 export default function Page() {
   return (
     <>
-      <PageTitle title="コース・プラン" subTitle="本格焼酎" />
+      <PageTitle title="お飲み物" subTitle="本格焼酎" />
       <div className="p-6 text-center">準備中</div>
     </>
   )
