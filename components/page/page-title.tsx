2ea052f5735c81ae8c5ebec632d13b08fa294001
taiccy <taishi.kpum@gmail.com>
2025-10-13 11:11:22 +0900

    fix: 左右にマージンを追加

--- components/page/page-title.tsx
+++ components/page/page-title.tsx
@@ -6,13 +6,13 @@ export default function PageTitle({
   subTitle: string
 }) {
   return (
-    <div className="my-10 text-center">
+    <div className="my-10 px-4 text-center">
       <div className="border-foreground/40 mb-2 inline-block border-b-1 pb-2">
-        <h1 className="text-foreground text-[clamp(1.5rem,4cqw,2.5rem)] tracking-wider">
+        <h1 className="text-muted-foreground text-[clamp(1.5rem,4cqw,2.5rem)] tracking-wider">
           {title}
         </h1>
       </div>
-      <p className="text-muted-foreground mx-auto text-[clamp(1.5rem,3cqw,2rem)] leading-relaxed">
+      <p className="text-foreground mx-auto text-[clamp(1.5rem,3cqw,2rem)] leading-relaxed text-pretty">
         {subTitle}
       </p>
     </div>
