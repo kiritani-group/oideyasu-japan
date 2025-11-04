afe1f5e04cf9d123af94c99fe54d391c35e57e41
taiccy <taishi.kpum@gmail.com>
2025-11-04 16:19:28 +0900

    feat: ページ内でセッション確認を追加

--- app/(admin)/admin/order/page.tsx
+++ app/(admin)/admin/order/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+  const userRole = session?.user.role
   return <div>注文リスト</div>
 }
