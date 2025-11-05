2d4b52da24ad9c8f2939e1ce6fe4f54446685d22
taiccy <taishi.kpum@gmail.com>
2025-11-05 14:30:34 +0900

    fix: usePathname を利用する Breadcrumbs をSuspense で囲う

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/sidebar"
 import type { Metadata } from "next"
 import { Noto_Sans_JP } from "next/font/google"
+import { Suspense } from "react"
 import Breadcrumbs from "./_breadcrumbs"
 
 const notoSansJp = Noto_Sans_JP({
@@ -37,7 +38,9 @@ export default function RootLayout({
           <SidebarInset>
             <header className="bg-background/50 sticky top-0 z-50 flex h-14 items-center gap-2 border-b px-2 backdrop-blur-sm">
               <AdminSidebarTrigger className="md:hidden" />
-              <Breadcrumbs />
+              <Suspense>
+                <Breadcrumbs />
+              </Suspense>
             </header>
             <main className="@container relative flex-1 bg-sky-200/10">
               <div className="relative z-10 mx-auto max-w-5xl">{children}</div>
