a31722d830b619531257dfec5847349d78b26e5a
taiccy <taishi.kpum@gmail.com>
2025-11-05 14:29:29 +0900

    feat: 顧客詳細ページの not-found を作成

---
+++ app/(admin)/admin/user/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function notFound() {
+  return (
+    <div className="flex justify-center p-2 pt-40">
+      <p className="text-sm text-muted-foreground">このユーザーは存在しないか削除済みです。</p>
+    </div>
+  )
+}
