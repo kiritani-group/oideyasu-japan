e67cdabee861b188b0ed83c0e169854d318a271f
taiccy <taishi.kpum@gmail.com>
2025-11-06 18:22:22 +0900

    chore: 画面幅が広いときの余白を調整

--- app/(shop)/catalog/page.tsx
+++ app/(shop)/catalog/page.tsx
@@ -1,18 +1,19 @@
+"use cache"
+
 import PageTitle from "@/components/page/page-title"
 import type { Metadata } from "next"
-import { products } from "@/data/products"
 import ItemsGrid from "./_components/items-grid"
 
 export const metadata: Metadata = {
   title: "通販くらぶ",
 }
 
-export default function Page() {
+export default async function Page() {
   return (
     <>
       <PageTitle title="お取り寄せ - 通販くらぶ" subTitle="商品一覧" />
-      <section className="mx-2 my-10">
-        <ItemsGrid items={products} />
+      <section className="mx-2 my-10 @xl:mx-4">
+        <ItemsGrid />
       </section>
     </>
   )
