04fab993c810882c1514eabcf71015eee4dc7351
taiccy <taishi.kpum@gmail.com>
2025-11-06 20:54:29 +0900

    feat: ゲストIDが付与されていない場合は、サーバーアクション内でセット

--- app/(shop)/catalog/[slug]/_components/_action.ts
+++ app/(shop)/catalog/[slug]/_components/_action.ts
@@ -1,4 +1,21 @@
+"use server"
+
+import { randomUUID } from "crypto"
+import { cookies } from "next/headers"
+
 export async function addCartAction(productId: string, quantity: number) {
+  const cookieStore = await cookies()
+  let guestId = cookieStore.get("guest_id")?.value
+
+  if (!guestId) {
+    guestId = randomUUID()
+    cookieStore.set("guest_id", guestId, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 60 * 60 * 24 * 30, // 30日
+    })
+  }
   await new Promise((resolve) => setTimeout(resolve, 2000))
   return { status: "SUCCESS" }
 }
