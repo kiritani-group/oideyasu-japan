20c16ed525ee017b3adf5704ad1220546bf56589
taiccy <taishi.kpum@gmail.com>
2025-12-15 14:30:57 +0900

    feat: id の生成を prisma に任せるように変更

--- lib/auth.ts
+++ lib/auth.ts
@@ -44,6 +44,9 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  advanced: {
+    database: { generateId: false },
+  },
   plugins: [
     anonymous(),
     magicLink({
